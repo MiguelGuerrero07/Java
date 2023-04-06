92b90eb623939090aaaeb7cd9abe2472e158b892
Miguel Guerrero <miguel.magr0202@gmail.com>
2023-04-06 15:05:37 -0600

    Ejemplos de variables de tipo entero , string y concatenación de las mismas

--- HolaMundoJava/src/HolaMundo.java
+++ HolaMundoJava/src/HolaMundo.java
@@ -3,5 +3,56 @@ public class HolaMundo {
     public static void main(String args []){
         System.out.println("Hola Mundo Java");
         
+        //Variables: Especificar el tipo de dato
+        
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
+        
+//Modificamos el valor de la variable y se puede reutilizar
+        miVariableEntera = 20;
+        System.out.println(miVariableEntera);
+        
+        
+        //Permite asigar cadenas de textos
+        String miVariableCadena = "Saludos";
+        System.out.println(miVariableCadena);
+        
+        miVariableCadena="Adios";
+        System.out.println(miVariableCadena);
+        
+        //Var -Inferencia de tipos en Java a partir de la version 10
+        var miVariableEntera2 = 15;
+        System.out.println(miVariableEntera2);
+        
+        var miVariableCadena2 = "Hola";
+        System.out.println(miVariableCadena2);
+        //Concatenar 
+        System.out.println("miVariableCadena2 = " + miVariableCadena2);
+        
+        //Reglas para definir variables ,Mini
+        /*
+        1.- Utilizar la primera letra en miniscula.
+        Ejemplo :   
+                String user = User;
+        2.- No se puede utilizar numero al inicio de la declaración.
+        Ejemplo:
+                int 1Mivariable = 1;
+        3.- Puede tener Guion Bajo ,nada común pero es permitido
+        Ejemplo:
+            _Mivariable3 = 3;
+            _mivariable5 = 5;
+        
+        4.-Se pude utilizar el simbolo de dolar
+        Ejemplo:
+            var $MiVariable4 = 4;
+        5.-No recomendanble usar acentos en la definición de variables
+        var canción = 20;|
+        */
+        
+        //Concatenación en Java
+        
+        
+        
+        
     }
 }
